b77cd1475ca67d85136350cbc6f007164af52281
Seth Hu <sethhu20@gmail.com>
2024-04-13 21:08:41 +1000

    Moved benchmark code

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
-
+compileargs = benchmark/benchmark-engine.cpp engine.cpp bitboardgame.cpp lookuptables.cpp -std=c++20
 
 a: *.cpp
 	g++ *.cpp -O3 -o a -std=c++20
+
+benchmark: benchmark/benchmark-engine.cpp engine.cpp bitboardgame.cpp lookuptables.cpp
+	g++ $(compileargs) -o benchmark/benchmark-engine
+	g++ $(compileargs) -O2 -o benchmark/benchmark-engine-O2
+	g++ $(compileargs) -O3 -o benchmark/benchmark-engine-O3
--- main.cpp
+++ main.cpp
@@ -105,39 +105,13 @@ void playEngine() {
   }
 }
 
-void benchmarkEngine() {
-  Engine engine;
-  BitboardGame game;
-  timespec base;
-  timespec_get(&base, TIME_UTC);
-  for (int i = 0; !game.gameOver(); ++i) {
-    timespec start;
-    timespec_get(&start, TIME_UTC);
-    Direction dir = engine.makeMove(game, 3);
-    if (dir == NONE) throw "L bozos"; 
-    game.makeMove(dir, true);
-    timespec end;
-    timespec_get(&end, TIME_UTC);
-    
-    cout << "Step " << i 
-         << " " << (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9 << "s "
-         << " " << (end.tv_sec - base.tv_sec) + (end.tv_nsec - base.tv_nsec) / 1e9 << "s "
-         << ((end.tv_sec - base.tv_sec) + (end.tv_nsec - base.tv_nsec) / 1e9) / (i + 1) << "s/step"
-         << endl;
-    // game.render();
-    // cout << game.gameOver() << endl;
-  }
-  game.render();
-}
-
 int main() {
   Lookup::generateLookupTables();
-  srand(333);
+  srand(time(NULL));
 
   //playGame();
   //playBitboardGame();
   //simulateGames();
   //simulateBitboardGames();
-  // playEngine();
-  benchmarkEngine();
+  playEngine();
 }
